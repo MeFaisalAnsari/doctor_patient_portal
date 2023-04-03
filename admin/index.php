25bcf365de414552809a7692c0d297840f8fa679
Faisal Ansari <me.developer.faisal@gmail.com>
2023-04-04 02:41:31 +0530

    updated dashboard of admin

--- admin/index.php
+++ admin/index.php
@@ -1,19 +1,25 @@
 <?php include("includes/header.php") ?>
 <?php
-$patient_query = mysqli_query($con, "SELECT * FROM patients");
-$patient_count = mysqli_num_rows($patient_query);
+$query = mysqli_query($con, "SELECT * FROM patients");
+$patient_count = mysqli_num_rows($query);
 
-$doctor_query = mysqli_query($con, "SELECT * FROM doctors WHERE status = '1'");
-$doctor_count = mysqli_num_rows($doctor_query);
+$query = mysqli_query($con, "SELECT * FROM doctors");
+$doctor_count = mysqli_num_rows($query);
 
-$appointment_query = mysqli_query($con, "SELECT * FROM appointments");
-$appointment_count = mysqli_num_rows($appointment_query);
+$query = mysqli_query($con, "SELECT * FROM appointments");
+$appointment_count = mysqli_num_rows($query);
+
+$query = mysqli_query($con, "SELECT * FROM blood_donation");
+$blood_donation_count = mysqli_num_rows($query);
+
+$query = mysqli_query($con, "SELECT * FROM organ_donation");
+$organ_donation_count = mysqli_num_rows($query);
 ?>
 
 <div class="container my-5">
     <h1><i class="fa-solid fa-dashboard me-2"></i> Dashboard</h1>
 </div>
-<div class="container">
+<div class="container mb-5">
     <div class="row">
         <div class="col-md-4 col-12">
             <div class="shadow rounded-4 border-start border-5 border-primary p-4">
@@ -39,6 +45,24 @@ $appointment_count = mysqli_num_rows($appointment_query);
                 <div class="d-flex justify-content-between">
                     <span class="fs-1 text-primary"><?php echo $appointment_count ?></span>
                     <i class="fa-solid fa-calendar-days fa-3x text-dark text-opacity-25"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-12 mt-4">
+            <div class="shadow rounded-4 border-start border-5 border-primary p-4">
+                <h5 class="text-uppercase">Blood Donations</h5>
+                <div class="d-flex justify-content-between">
+                    <span class="fs-1 text-primary"><?php echo $blood_donation_count ?></span>
+                    <i class="fa-solid fa-hand-holding-droplet fa-3x text-dark text-opacity-25"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-12 mt-4">
+            <div class="shadow rounded-4 border-start border-5 border-primary p-4">
+                <h5 class="text-uppercase">Organ Donations</h5>
+                <div class="d-flex justify-content-between">
+                    <span class="fs-1 text-primary"><?php echo $organ_donation_count ?></span>
+                    <i class="fa-solid fa-hand-holding-medical fa-3x text-dark text-opacity-25"></i>
                 </div>
             </div>
         </div>
